a8a1c20be7985b080c3cb8f316f8769ba4fa5740
benyapakanon <benyapakanon503@gmail.com>
2019-08-28 16:52:33 +0700

    sharewegobar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ButtonBack from "./components/button-back";
+
+function App() {
+  return (
+    <React.Fragment>
+      <ButtonBack></ButtonBack>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/button-back.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+
+class ButtonBack extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                
+                <button><ArrowBackIcon>start</ArrowBackIcon></button>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ButtonBack;
